bd99c404f0d39f5166b9e945b5e9193b5d484da3
Nicholas McDonald <nmcdonaldfhs@gmail.com>
2022-09-08 20:48:50 -0700

    Add some documentation to convenience initializer

--- Sources/nickdonald-boilerplate/Combine/Sink+Extensions.swift
+++ Sources/nickdonald-boilerplate/Combine/Sink+Extensions.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  Sink+Extensions.swift
 //  
 //
 //  Created by Nicholas McDonald on 9/8/22.
@@ -8,6 +8,8 @@
 import Combine
 
 extension Subscribers.Sink {
+    /// Convenience initializer for when you don't care about the completion of a publisher
+    /// - Parameter receiveValue: The closure to execute on receipt of a value.
     public convenience init(receiveValue: @escaping (Input) -> Void) {
         self.init { _ in
             // Do nothing...
